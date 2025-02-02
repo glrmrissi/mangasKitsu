3e78f3ecd4a5ddac97de56faf54ac2ce7d16b7ac
glrmrissi <guilhermerissi04112006@gmail.com>
2025-02-02 18:00:04 -0300

    few changes in details, more details added

--- src/pages/details/js/script.js
+++ src/pages/details/js/script.js
@@ -1,38 +1,43 @@
 async function fetchMangaDetails(id) {
-    const response = await fetch(`https://kitsu.io/api/edge/manga/${id}`);
-    const data = await response.json();
-    return data.data;
+  const response = await fetch(`https://kitsu.io/api/edge/manga/${id}`);
+  const data = await response.json();
+  return data.data;
+}
+
+async function displayMangaDetails() {
+  const mangaId = localStorage.getItem('selectedAnimeId');
+  if (!mangaId) {
+    document.body.innerHTML = '<p>Erro: Nenhum mangá selecionado.</p>';
+    return;
   }
 
-  async function displayMangaDetails() {
-    const mangaId = localStorage.getItem('selectedAnimeId');
-    if (!mangaId) {
-      document.body.innerHTML = '<p>Erro: Nenhum mangá selecionado.</p>';
-      return;
-    }
-
-    const manga = await fetchMangaDetails(mangaId);
-    const mangaDetails = document.getElementById('manga-details');
-
-    document.title = `Details ${manga.attributes.titles.en || manga.attributes.titles.jp || manga.attributes.canonicalTitle || manga.attributes.titles.en_jp || "Don't have title"} - Rissi`;
-
-    mangaDetails.innerHTML = `
+  
+  const manga = await fetchMangaDetails(mangaId);
+  const mangaDetails = document.getElementById('manga-details');
+  const {coverImage, posterImage, titles, canonicalTitle, status, averageRating, volumeCount, synopsis, userCount } = manga.attributes
+  const statusUpperCase = status.charAt(0).toUpperCase() + status.slice(1);
+  document.title = `Details ${titles.en || titles.jp || canonicalTitle || titles.en_jp || "Don't have title"} - Rissi`;
+  
+  mangaDetails.innerHTML = `
     <div class="banner_manga">
-      <img src="${manga.attributes.coverImage?.original || manga.attributes.posterImage.original || manga.attributes.posterImage.large || manga.attributes.posterImage.small}" />
+      <img src="${coverImage?.original || posterImage.original || posterImage.large || posterImage.small}" />
       </div>
       <section class="grid-manga-details">
         <aside class="left-side d-center">
-          <h2>${manga.attributes.titles.en || manga.attributes.titles.en_jp || manga.attributes.canonicalTitle || manga.attributes.titles.ja_jp || "Content not found"}</h2>
-          <img class="img-details" src="${manga.attributes.posterImage.large}" alt="${manga.attributes.titles.en || manga.attributes.titles.en_jp || manga.attributes.titles.ja_jp}" />
-          <p><strong>Status:</strong>&nbsp; ${manga.attributes.status}</p>
-          <p><strong>Rating:</strong>&nbsp; ${manga.attributes.averageRating}</p>
-          <p><strong>Chapters:</strong>&nbsp; ${manga.attributes.volumeCount}</p>
+          <h2>${titles.en || titles.en_jp || canonicalTitle || titles.ja_jp || "Content not found"}</h2>
+          <img class="img-details" src="${posterImage.large}" alt="${titles.en || titles.en_jp || titles.ja_jp}" />
+          <div class="infos-details">
+            <p><strong>Status:</strong>&nbsp; ${statusUpperCase || "Content not found"}</p>
+            <p><strong>Views:</strong>&nbsp; ${userCount || "Content not found"}</p>
+            <p><strong>Rating:</strong>&nbsp; ${averageRating || "Content not found"}</p>
+            <p><strong>Chapters:</strong>&nbsp; ${volumeCount || "Content not found"}</p>
+          </div>
           </aside>
           <aside class="right-side d-center">
-            <p>&nbsp; &nbsp;${manga.attributes.synopsis || "Content not found"}</p>
+            <p>&nbsp; &nbsp;${synopsis || "Content not found"}</p>
             </aside>
             </section>
             `;
-  }
+}
 
-  displayMangaDetails();
+displayMangaDetails();
