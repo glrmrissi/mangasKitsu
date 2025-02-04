5c0128bec6a4f2faaa387947dc040f10ca5e85d9
glrmrissi <guilhermerissi04112006@gmail.com>
2025-02-03 21:29:17 -0300

    ops, i forget this

---
+++ src/js/components/fadeInOut/fadeInOut.js
@@ -0,0 +1,11 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
+        }
+    });
+}, { threshold: 0.1 });
+
+document.querySelectorAll(".box").forEach(box => observer.observe(box));
