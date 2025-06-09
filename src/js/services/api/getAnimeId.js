fd404f2197d6f375bb5e28bc4a0e6ae3225baddc
glrmrissi <guilhermerissi04112006@gmail.com>
2025-06-09 02:06:16 -0300

    fix: solved details url

--- src/js/services/api/getAnimeId.js
+++ src/js/services/api/getAnimeId.js
@@ -1,7 +1,7 @@
 
 export function getAnime(mangaItem, mangaId) {
     mangaItem.addEventListener("click", () => {
-        const baseURL = location.hostname === "127.0.0.1" || location.hostname === "localhost" ? "/" : "/mangasKitsu";
+        const baseURL = location.hostname === "127.0.0.1" || location.hostname === "localhost" ? "/" : "/mangasKitsu/";
         const detailsUrl = `${baseURL}src/pages/details/details-manga.html?id=${encodeURIComponent(mangaId)}`
 
         window.open(detailsUrl, '_blank');
