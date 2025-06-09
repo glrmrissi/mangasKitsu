14cf73a1b2804e7403846dc549e27d2b515a1131
glrmrissi <guilhermerissi04112006@gmail.com>
2025-06-09 01:47:06 -0300

    testing: url of details manga #1

--- src/js/services/api/getAnimeId.js
+++ src/js/services/api/getAnimeId.js
@@ -1,7 +1,7 @@
 
 export function getAnime(mangaItem, mangaId) {
     mangaItem.addEventListener("click", () => {
-        const detailsUrl = `../../../src/pages/details/details-manga.html?id=${encodeURIComponent(mangaId)}`
+        const detailsUrl = `../../src/pages/details/details-manga.html?id=${encodeURIComponent(mangaId)}`
 
         window.open(detailsUrl, '_blank');
         return mangaId
