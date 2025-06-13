b83ca6c665744058c8b67de2bfa80bb003fb98ae
glrmrissi <guilhermerissi04112006@gmail.com>
2025-06-13 14:31:11 -0300

    fix: title fixed and more links added, and mini-api on github

--- src/js/pages/mangas/manga.js
+++ src/js/pages/mangas/manga.js
@@ -1,32 +1,57 @@
 import { fetchApi } from "../../services/api/fetchApi.js";
 import { getAnime } from "../../services/api/getAnimeId.js";
 
+let url;
 export const fetchMangasPage = async () => {
-  const url = "https://kitsu.io/api/edge/manga?page[limit]=10&filter[subtype]=manga&filter[categories]=action";
+  fetch("../../links/links.json")
+    .then(response => response.json())
+    .then(data => {
+      data.linksMangas.forEach((link) => {
+        url = link;
+        loadSectionsMangas(url);
+      })
+    });
+};
+
+function loadSectionsMangas(url) {
   const mainManga = document.querySelector("#mainManga")
   fetchApi(url)
     .then((data) => {
       const mangaDiv = document.createElement("section");
       const titleCategory = document.createElement("h2");
 
-      titleCategory.textContent = "Action"
+      const categoriesTitleUrl = data.data[0].relationships.categories.links.related;
 
-      mainManga.appendChild(titleCategory)
+      mainManga.appendChild(titleCategory);
+
+      fetch(categoriesTitleUrl)
+        .then(response => response.json())
+        .then(data => {
+          titleCategory.textContent = data.data[0].attributes.title;
+          // console.log(`Categoria: ${data.data[0].attributes.title}`)
+        })
+        .catch(error => {
+          console.error("Erro ao buscar categorias:", error);
+        });
+
+
       data.data.forEach((manga) => {
-        const { canonicalTitle, synopsis, posterImage, ageRating} =
-          manga.attributes;
+        const { canonicalTitle, synopsis, posterImage, ageRating } = manga.attributes;
 
         const mangaItem = document.createElement("div");
         const title = document.createElement("h3");
         const img = document.createElement("img");
 
         mangaDiv.classList.add("manga-div")
+        mangaItem.classList.add("manga-item")
+        title.classList.add("text-overlay")
         title.textContent = canonicalTitle;
+
         img.src = posterImage.small;
         img.title = canonicalTitle;
 
-        mangaItem.appendChild(title)
         mangaItem.appendChild(img);
+        mangaItem.appendChild(title)
         mangaDiv.appendChild(mangaItem);
         mainManga.appendChild(mangaDiv);
 
@@ -36,7 +61,7 @@ export const fetchMangasPage = async () => {
     .catch((error) => {
       console.error(error);
     });
-};
+}
 
 
 fetchMangasPage();
