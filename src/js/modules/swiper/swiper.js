b1dc0a6757d27536bb9b5a45c45f2fd9b2f5753d
glrmrissi <guilhermerissi04112006@gmail.com>
2025-06-28 19:56:09 -0300

    style: slider have fade effect

--- src/js/modules/swiper/swiper.js
+++ src/js/modules/swiper/swiper.js
@@ -1,7 +1,8 @@
 const swiper = new Swiper('.swiper', {
     loop: true,
     slidesPerView: 1,
-    spaceBetween: 20,
+    spaceBetween: 0,
+    effect: "fade",
 
     pagination: {
         el: '.swiper-pagination',
