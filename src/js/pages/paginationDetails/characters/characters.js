a911e3d84484af8c20ed5622ea35ee71dcd4d04a
glrmrissi <guilhermerissi04112006@gmail.com>
2025-07-13 18:08:20 -0300

    fix: image error fixed

--- src/js/pages/paginationDetails/characters/characters.js
+++ src/js/pages/paginationDetails/characters/characters.js
@@ -1,81 +1,85 @@
 const divCharacters = document.getElementById("characters");
 const charactersGrid = document.createElement("div");
+const loadMore = document.createElement("button");
+
 charactersGrid.classList.add("grid_characters");
 
-const loadMore = document.createElement("button")
 let mangaId = new URL(location);
-let offset = 0;
 mangaId = mangaId.searchParams.get("id");
 
-async function x() {
+let offset = 0;
+
+async function loadCharacters() {
     showLoading();
-    let url = `https://kitsu.io/api/edge/manga/${mangaId}/characters?include=character&page[offset]=${offset}`;
-    console.log(url)
+    let url = `https://kitsu.io/api/edge/manga/${mangaId}/characters?include=character&page[offset]=${offset}`
     await fetch(url)
         .then(response => response.json())
         .then(data => {
-            console.log(data.included)
-            const datas = data.included
+            const datas = data.included;
 
-            // TODO: ARRUMAR ISSO AQUI, QUE ESTÁ RETORNANDO UM MONTE DE VEZES A FOTO DO GATO 
-            const h1Error = document.createElement("h1");
-            h1Error.textContent = "Not Found Characters";
-
-            const imageError = document.createElement("img");
-            imageError.src = "../../../src/imgs/confuseCat.png";
-            imageError.style.width = "500px";
-            imageError.title = "Not found characters";
-            imageError.alt = "Not found characters";
-            imageError.classList.add("img-characters");
-
-            if (datas === undefined) {
-                if (divCharacters.contains(h1Error)) {
-                    divCharacters.removeChild(h1Error);
-                    divCharacters.removeChild(imageError);
-                } else {
-                    divCharacters.appendChild(h1Error);
-                    divCharacters.appendChild(imageError);
-                }
-                hideLoading();
-            }
-            
             datas.forEach((characters) => {
                 const imgsUrl = characters?.attributes?.image?.original;
                 const divImgCharacters = document.createElement("article");
                 const image = document.createElement("img");
-                const titleCharacter = document.createElement("p")
-                
+                const titleCharacter = document.createElement("p");
+
                 image.classList.add("img-characters");
                 divImgCharacters.classList.add("div-grid-characters");
-                titleCharacter.classList.add("text-overlay")
-                
+                titleCharacter.classList.add("text-overlay");
+
                 image.src = imgsUrl;
-                
+
                 titleCharacter.textContent = characters.attributes?.names?.en || characters.attributes?.names?.ja_jp;
                 if (imgsUrl != null || imgsUrl != undefined) {
-                    divCharacters.textContent = ""
+                    divCharacters.textContent = "";
                     divImgCharacters.appendChild(image);
-                    divImgCharacters.appendChild(titleCharacter)
-                    charactersGrid.appendChild(divImgCharacters)
+                    divImgCharacters.appendChild(titleCharacter);
+                    charactersGrid.appendChild(divImgCharacters);
                     divCharacters.appendChild(charactersGrid);
                 }
             });
             let ultimo = datas[datas.length - 1]
             if (ultimo === datas[datas.length - 1]) {
-                loadMore.textContent = "Load more characters"
-                divCharacters.appendChild(loadMore)
+                loadMore.textContent = "Load more characters";
+                divCharacters.appendChild(loadMore);
+                console.log("created")
             } else {
-                console.info("Last child not found")
+                console.info("Last child not found");
             }
-            console.log(ultimo)
             hideLoading();
         })
-        .catch(error => console.error("Erro ao buscar os personagens:", error));
+        .catch(error => {
+            allContentLoad();
+        });
+}
+
+function allContentLoad() {
+    const h2Error = document.createElement("h2");
+
+    h2Error.id = "h2_error";
+
+    const imageError = document.createElement("img");
+    imageError.src = "../../../src/imgs/confuseCat.png";
+    imageError.style.width = "500px";
+    imageError.title = "Not found characters";
+    imageError.alt = "Not found characters";
+    imageError.classList.add("img-characters");
+    h2Error.textContent = "Not found characters";
+
+    if (divCharacters.contains(document.getElementById("h2_error"))) {
+        console.log("Not found characters!")
+    } else {
+        divCharacters.appendChild(h2Error);
+        divCharacters.appendChild(imageError);
+        hideLoading();
     }
+    hideLoading();
+}
+
 
 loadMore.onclick = function () {
     offset += 10;
-    x();
+    loadCharacters();
 }
 
-x();
+loadCharacters();
