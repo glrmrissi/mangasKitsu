75c5079d84d518cb33d9f0d838397104fdd004f5
glrmrissi <guilhermerissi04112006@gmail.com>
2025-07-14 19:00:21 -0300

    fix: pathing

--- src/js/services/api/getAnimeId.js
+++ src/js/services/api/getAnimeId.js
@@ -1,7 +1,12 @@
 
 export function getAnime(mangaItem, mangaId) {
     mangaItem.addEventListener("click", () => {
-        const baseURL = location.hostname === "127.0.0.1" || location.hostname === "localhost" ? "/" : "/mangasKitsu/";
+        const pathParts = location.pathname.split("/").filter(Boolean);
+        const firstFolder = pathParts.length > 0 ? pathParts : "";
+
+        const baseURL = firstFolder[0] === "src" || firstFolder[1] === "src"
+            ? `../../../`
+            : "";
         const detailsUrl = `${baseURL}src/pages/details/details-manga.html?id=${encodeURIComponent(mangaId)}`
 
         window.open(detailsUrl, '_blank');
