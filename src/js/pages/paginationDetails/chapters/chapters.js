7d68619d43688a8c315c7306a573a295d20ac8c4
glrmrissi <guilhermerissi04112006@gmail.com>
2025-07-16 20:05:55 -0300

    feat: chapters added, for now ins't done

--- src/js/pages/paginationDetails/chapters/chapters.js
+++ src/js/pages/paginationDetails/chapters/chapters.js
@@ -1,6 +1,53 @@
 // https://kitsu.io/api/edge/chapters
-fetch("https://kitsu.io/api/edge/chapters/38")
-.then(response => response.json())
-.then(data => {
-    // console.log(data)
-})
+
+const mainDiv = document.querySelector("#chapters")
+
+mainDiv.classList.add("grid-launcher")
+
+let mangaId = new URL(location);
+let offset = 10;
+mangaId = mangaId.searchParams.get("id");
+
+function load() {
+
+    fetch(`https://kitsu.io/api/edge/manga/${mangaId}/chapters?page[offset]=${offset}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            data.data.forEach(chapters => {
+                const title = chapters.attributes.canonicalTitle;
+                const thumbnail = chapters.attributes.thumbnail;
+
+                const contentNotFound = "../../../src/imgs/cntf-1.png";
+
+                const titlespan = document.createElement("span");
+                const img = document.createElement("img");
+                const containerImgTitle = document.createElement("div");
+
+                containerImgTitle.classList.add("div-grid-characters");
+
+                img.classList.add("img-characters");
+
+                titlespan.textContent = title;
+                img.src = thumbnail || contentNotFound;
+
+                console.log(title)
+                console.log(thumbnail)
+
+                if (title != null) {
+                    containerImgTitle.appendChild(titlespan)
+                    containerImgTitle.appendChild(img)
+                    mainDiv.appendChild(containerImgTitle)
+                }
+            });
+        })
+        .catch(e => {
+            console.error(e)
+        })
+}
+load()
+function x() {
+    offset += 10;
+    load()
+}
+x()
